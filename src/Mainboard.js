16621b1fb25c902d15415f503d8162acc4dd6c44
vistritpandey <vistritpandey@gmail.com>
2021-03-03 08:31:55 +0530

    Mainboard added

---
+++ src/Mainboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mainboard.css";
+
+function Mainboard() {
+  return (
+    <div className="mainboard">
+      <h1>Mainboard</h1>
+    </div>
+  );
+}
+
+export default Mainboard;
