7b5c8b1ddcf46292d22042f620b8e646dce1b0ed
vistritpandey <vistritpandey@gmail.com>
2021-03-03 08:49:43 +0530

    Noice

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,41 @@
 import React from "react";
-import "./Header.css";
+import styled from "styled-components";
+import { IconButton } from "@material-ui/core";
+import PinterestIcon from "@material-ui/icons/Pinterest";
 
 function Header() {
   return (
-    <div className="header">
-      <h1>Header</h1>
-    </div>
+    <Wrapper>
+      <LogoWrapper>
+        <IconButton>
+          <PinterestIcon />
+        </IconButton>
+      </LogoWrapper>
+      {/*<HomePageButton></HomePageButton>
+      <FollowingButton></FollowingButton>
+      <SearchWrapper>
+        <SearchBarWrapper></SearchBarWrapper>
+      </SearchWrapper>
+      <IconWrapper></IconWrapper>*/}
+    </Wrapper>
   );
 }
 
 export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
