44a96c245006f5bc0a85813639ee5470232841ee
vistritpandey <vistritpandey@gmail.com>
2021-03-03 10:17:44 +0530

    k

--- src/Mainboard.js
+++ src/Mainboard.js
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function Mainboard() {
   return (
-    <div className="mainboard">
-      <h1>Mainboard</h1>
-    </div>
+    <Wrapper>
+      <Container></Container>
+    </Wrapper>
   );
 }
 
 export default Mainboard;
+
+const Wrapper = styled.div`
+  background-color: white;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  margin-top: 15px;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 80%;
+  background-color: white;
+`;
