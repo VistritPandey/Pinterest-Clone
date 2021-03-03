6ba6c973c76e84e5d3419c6fbd5df308bcdca082
vistritpandey <vistritpandey@gmail.com>
2021-03-03 08:36:58 +0530

    Material UI added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Mainboard from "./Mainboard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Mainboard />
     </div>
   );
 }
