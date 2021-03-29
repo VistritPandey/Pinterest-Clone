5956b92163fa5ced6a893d7d3fb5c8941503bd7f
vistritpandey <vistritpandey@gmail.com>
2021-03-29 19:06:11 +0530

    Done!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
 import "./App.css";
 import Header from "./Header";
 import Mainboard from "./Mainboard";
-import api from "./api"
-import { useState } from "react";
+import api from "./api";
+import { useState, useEffect } from "react";
+import db from "./firebase";
 
 function App() {
-  const [pins, setNewPins] = useState([])
+  const [pins, setNewPins] = useState([]);
 
   const getImages = (term) => {
     return api.get("https://api.unsplash.com/search/photos", {
       params: {
-        query: term
-      }
-    })
-  }
+        query: term,
+      },
+    });
+  };
 
   const onSearchSubmit = (term) => {
-    console.log("ok boomer" , term)
-      getImages(term).then((res) => {
-        let results = res.data.results;
-        let newPins = [
-          ...results,
-          ...pins,
-        ]
+    getImages(term).then((res) => {
+      let results = res.data.results;
+      let newPins = [...results, ...pins];
+      newPins.sort(function (a, b) {
+        return 0.5 - Math.random();
+      });
+      setNewPins(newPins);
+    });
+  };
 
-        newPins.sort(function(a,b) {
-          return 0.5 - Math.random();
-        });
-        setNewPins(newPins)
-      })
-  }
+  const getNewPins = () => {
+    let promises = [];
+    let pinData = [];
 
+    let pins = ["Beach", "India", "Cars", "smartphones"];
+    pins.forEach((pinTerm) => {
+      promises.push(
+        getImages(pinTerm).then((res) => {
+          let results = res.data.results;
+          console.log(results);
+          pinData = pinData.concat(results);
+          pinData.sort(function (a, b) {
+            return 0.5 - Math.random();
+          });
+        })
+      );
+    });
+    Promise.all(promises).then(() => {
+      setNewPins(pinData);
+    });
+  };
+
+  useEffect(() => {
+    getNewPins();
+  }, []);
 
   return (
     <div className="App">
-      <Header onSubmit={onSearchSubmit}/>
+      <Header onSubmit={onSearchSubmit} />
       <Mainboard pins={pins} />
     </div>
   );
