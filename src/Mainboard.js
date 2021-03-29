108f4d69485e8e727a2b9094da134c12faf1d1be
vistritpandey <vistritpandey@gmail.com>
2021-03-29 18:52:49 +0530

    k

--- src/Mainboard.js
+++ src/Mainboard.js
@@ -1,35 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import Pin from "./Pin";
+import "./Mainboard.css";
 
 function Mainboard(props) {
   let { pins } = props;
 
   return (
-    <Wrapper>
-      <Container>
+    <div className="mainboard">
+      <div className="mainboard__container">
         {pins.map((pin) => {
           let { urls } = pin;
-          <Pin urls={urls} />;
+          return <Pin urls={urls} />;
         })}
-      </Container>
-    </Wrapper>
+      </div>
+    </div>
   );
 }
 
 export default Mainboard;
-
-const Wrapper = styled.div`
-  background-color: white;
-  display: flex;
-  width: 100%;
-  height: 100%;
-  margin-top: 15px;
-  justify-content: center;
-`;
-
-const Container = styled.div`
-  display: flex;
-  width: 80%;
-  background-color: white;
-`;
