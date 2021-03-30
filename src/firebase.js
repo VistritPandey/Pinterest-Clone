80c1cce97a526e782714c5692f49329aee8f3f81
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-03-30 17:02:20 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtQqoW4-vpQ43jzvicGDWQLmebphCWYo8",
-  authDomain: "vistrit-pinterest.firebaseapp.com",
-  projectId: "vistrit-pinterest",
-  storageBucket: "vistrit-pinterest.appspot.com",
-  messagingSenderId: "363880974457",
-  appId: "1:363880974457:web:716039f647a7bbeb0c2ca8",
-  measurementId: "G-JPWFD9W2RM",
+  //use your own config
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
